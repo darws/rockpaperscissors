6de3fa689188532cf53c1e6ed68485d7cb08c1b7
dawrin <dawrin.pichardo13@gmail.com>
2018-11-19 23:07:20 +0000

    rock it

--- js/script.js
+++ js/script.js
@@ -1,18 +1,45 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
-
 //GLOBAL VARIABLES
 /* global $ */
 
-
+var userChoice;
+var computerChoice;
+var winner;
+var randomNumber;
 
 // DOCUMENT READY FUNCTION BELOW
 
-$("buttom").click(function() {
-   $("#userChoice").text($("#input").val());
+$("button").click(function() {
+   
+    userChoice=$("#input").val();
+    
+    $("#userChoice").text(userChoice);
 });
 
-$("buttom").click(function() {
-   $("#computerChoice").text($("#input").val());
+$("button").click(function() {
+   
+  // computerChoice=$("#input").val();
+   
+   //$("#computerChoice").text($("#input").val());
+   
+   
+   var randomNumber= Math.random();
+   
+console.log(randomNumber);
+if(randomNumber< .3333333) {
+   computerChoice = "piedra"
+        $("#computerChoice").html("piedra"); 
+    }
+ else if(randomNumber <  .6666666 ){
+    computerChoice = " tijeras" 
+ $("#computerChoice").html("tijeras"); }
+ 
+ 
+ else {
+ $("#text").html("paper"); }
+ 
+
 });
+
